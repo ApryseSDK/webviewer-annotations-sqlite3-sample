4bdc304e3a642cd853a89116d2b66c9c4a2ab1d8
achenPDFTron <54084743+achenPDFTron@users.noreply.github.com>
2019-12-20 12:20:57 -0800

    Update for new 6.0 API changes (#3)

--- client/index.js
+++ client/index.js
@@ -10,14 +10,15 @@ initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
   var annotManager = docViewer.getAnnotationManager();
 
   // Save when annotation change event is triggered (adding, modifying or deleting of annotations)
-  annotManager.on('annotationChanged', function(e, annots) {
+  annotManager.on('annotationChanged', function(annots, action, options) {
     // If the event is triggered by importing then it can be ignored
     // This will happen when importing the initial annotations from the server or individual changes from other users
-    if (e.imported) return;
+    if (options.imported) return;
 
-    const xfdfStrings = annotManager.getAnnotCommand();
-    annots.forEach(function(annot) {
-      savexfdfString(DOCUMENT_ID, annot.Id, xfdfStrings);
+    annotManager.exportAnnotCommand().then(function (xfdfStrings) {
+      annots.forEach(function(annot) {
+        savexfdfString(DOCUMENT_ID, annot.Id, xfdfStrings);
+      });
     });
   });
 
@@ -25,8 +26,9 @@ initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
   docViewer.on('documentLoaded', function() {
     loadxfdfStrings(DOCUMENT_ID).then(function(rows) {
       JSON.parse(rows).forEach(col => {
-        const annotations = annotManager.importAnnotCommand(col.xfdfString);
-        annotManager.drawAnnotationsFromList(annotations);
+        annotManager.importAnnotCommand(col.xfdfString).then(function(annotations) {
+          annotManager.drawAnnotationsFromList(annotations);
+        });
       });
     });
   });
