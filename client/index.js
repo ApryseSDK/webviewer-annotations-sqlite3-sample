2df1303d8ed13d2c193b105a01793bb5f772028c
achenPDFTron <achen@pdftron.com>
2019-12-20 11:51:36 -0800

    changed getAnnotCommand to exportAnnotCommand

--- client/index.js
+++ client/index.js
@@ -15,7 +15,7 @@ initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
     // This will happen when importing the initial annotations from the server or individual changes from other users
     if (options.imported) return;
 
-    annotManager.getAnnotCommand().then(function (xfdfStrings) {
+    annotManager.exportAnnotCommand().then(function (xfdfStrings) {
       annots.forEach(function(annot) {
         savexfdfString(DOCUMENT_ID, annot.Id, xfdfStrings);
       });
